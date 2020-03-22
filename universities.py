9bb693f1ebc0b98209b53c43fef5626e9af85952
Bhushan Suwal <bhushan_suwal@live.com>
2020-03-22 16:14:16 -0400

    filter viable colleges

--- universities.py
+++ universities.py
@@ -1,8 +1,32 @@
 import pandas as pd
 
-def preprocess_universities(universities_file:                        min_endowment: float,
-                            miuniversities_file    universities_df = pd.read_csv(universities_file)
-    
-    universities_df["endowment"] = universities_df['endowment'].str
-    universities_df.astype({"endowmen"})[1:]
-    universities_df = universities_df[(collemin_dorm_beds)ges_df["endowment"] > min_endowment) ]
+def preprocess_colleges(colleges_file: str,
+                        min_endowment: float,
+                        min_dorm_capacity:float):
+
+    colleges_df = pd.read_csv(colleges_file)
+
+    def clean_numbers(string:str):
+        # for the NaN values
+        if type(string) == float:
+            return string
+
+        new_str = ""
+        for char in string:
+            if char.isnumeric():
+                new_str += char
+
+        if new_str == "":
+            return 0
+        else:
+            return (new_str)
+
+    colleges_df["endowment"] = colleges_df["endowment"].map(clean_numbers)
+    colleges_df["dorm capacity"] = colleges_df["dorm capacity"].map(clean_numbers)
+
+    colleges_df = colleges_df[(colleges_df["endowment"] > min_endowment) & (colleges_df["dorm capacity"] > min_dorm_capacity)]
+
+    return colleges_df
+
+df = preprocess_colleges("./data/ma_dorm_capacity.csv", 100000000, 500)
+print(df)
